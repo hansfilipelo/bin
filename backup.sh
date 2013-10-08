446b9726094202b99ef02e54a713aa8424e96ef1
hansfilipelo <hansfilipelo@gmail.com>
2013-10-08 19:53:05 +0200

    Changed so backup now runs on lagret instead of server02 due to problems with OpenVPN and default route

--- backup.sh
+++ backup.sh
@@ -4,6 +4,8 @@ LOGG=/home/fille/loggar/backup.txt
 touch $LOGG
 exec >> $LOGG 2>&1
 
+source backup.conf
+
 echo " "
 echo "Written by /home/fille/backup.sh"
 echo "-------------------------------------"
@@ -13,7 +15,7 @@ date "+%Y-%m-%d %H:%M"
 # Backup of main storage
 for FS in lagret privat os
 do
-	rsync -q -a --update --delete --exclude=".*" /mnt/storage/$FS/* root@lagret:/mnt/storage/$FS/
+	rsync -q -a --update --delete --exclude=".*" root@$REMOTEHOST:/mnt/storage/$FS/ /mnt/storage/$FS/*
 	
 	if [ $? == 0 ]
 	then
