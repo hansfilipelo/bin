1124806ed47990c6903f9fab2528548c058169bd
Hans-Filip Elo <fille@Hans-Filips-MacBook-Pro.local>
2013-10-29 22:07:38 +0100

    Scripts for mounting and unmounting TDDC76_labbar on MacBook

---
+++ mount_TDDC76_labbar.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#  mount_TDDC76_labbar.sh
+#  
+#
+#  Created by Hans-Filip Elo on 2013-10-29.
+#
+
+mkdir -p ~/Documents/TDDC76_labbar
+
+sshfs hanel742@ssh.cyd.liu.se:TDDC76_labbar ~/Documents/TDDC76_labbar
---
+++ unmount_TDDC76_labbar.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#  unmount_TDDC76_labbar.sh
+#  
+#
+#  Created by Hans-Filip Elo on 2013-10-29.
+#
+
+umount ~/Documents/TDDC76_labbar
+rm -r ~/Documents/TDDC76_labbar
