696394844a03cc0f09e67ad76e875c2f32748a47
hansfilipelo <hansfilipelo@gmail.com>
2014-06-05 15:13:19 +0200

    Works for uninitialized dekstops

--- showHideDesktop.sh
+++ showHideDesktop.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-STATUS=$(defaults read com.apple.finder | grep CreateDesktop | sed 's/[^0-9]//g')
+defaults read com.apple.finder | grep CreateDesktop > /dev/null
 
-if [ $STATUS == 0 ]
+if [ $? == 0 ]
 then
-	defaults write com.apple.finder CreateDesktop -bool true
-	killall Finder
+	
+	STATUS=$(defaults read com.apple.finder | grep CreateDesktop | sed 's/[^0-9]//g')
+	
+	if [ $STATUS == 0 ]
+	then
+		defaults write com.apple.finder CreateDesktop -bool true
+		killall Finder
+	else
+		defaults write com.apple.finder CreateDesktop -bool false
+		killall Finder
+	fi
 else
-	defaults write com.apple.finder CreateDesktop -bool false
+	defaults write com.apple.finder CreateDesktop -bool true
 	killall Finder
 fi
 
