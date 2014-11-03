baf23e83194bea4732078c984b7f47abd63a3c27
hansfilipelo <hansfilipelo@gmail.com>
2014-11-03 14:30:34 +0100

    Typo

--- backupMini.sh
+++ backupMini.sh
@@ -6,6 +6,6 @@ echo "---------------"
 rsync -raz --update --delete fille@10.0.0.25:~ /mnt/storage/home
 echo "---------------"
 echo "Finished"
-echo $(date '+%Y-%m-&d %H:%M')
+echo $(date '+%Y-%m-%d %H:%M')
 
 
