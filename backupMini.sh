9d7376c9d5f96e5d9b1f421a6c86c467efed6e84
hansfilipelo <hansfilipelo@gmail.com>
2014-11-03 13:51:11 +0100

    typo

--- backupMini.sh
+++ backupMini.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting backup..."
-echo $(date '+%Y-%m-&d %H:%M')
+echo $(date '+%Y-%m-%d %H:%M')
 echo "---------------"
 rsync -raz --update --delete fille@10.0.0.25:~ /mnt/storage/home
 echo "---------------"
