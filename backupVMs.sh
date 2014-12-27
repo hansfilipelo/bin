77e47d4c28a83aec0926c0320f1176ac7f09ccc0
Hans-Filip Elo <fille@miniserver.hfelo.se>
2014-12-27 19:05:58 +0100

    VM backup

---
+++ backupVMs.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+locations="/var/lib/libvirt/images /ssd2"
+
+echo "Shutting down VMs"
+date '+%Y-%m-%d %H:%M'
+echo "--------------"
+
+
+for domain in $(virsh -c qemu:///system list | tail -n +3 | grep \n | awk {'print $2'})
+do
+	virsh -c qemu:///system shutdown $domain
+done
+
+echo ""
+echo "Rsyncing VMs"
+date '+%Y-%m-%d %H:%M'
+echo "--------------"
+
+for location in $locations
+do
+	rsync -raz --update --delete $location server02:/mnt/storage/vms/
+done
+
+
+echo ""
+echo "Starting VMs"
+date '+%Y-%m-%d %H:%M'
+echo "--------------"
+
+
+for domain in $(virsh -c qemu:///system list | tail -n +3 | grep \n | awk {'print $2'})
+do
+	virsh -c qemu:///system start $domain
+done
+
+echo ""
+echo "Done!"
+date '+%Y-%m-%d %H:%M'
+
