01470b3c5357c9df2140ae36717fcdf4b7599180
hansfilipelo <hansfilipelo@gmail.com>
2015-09-26 15:34:09 +0200

    Exclude file for backup

--- backupClient.sh
+++ backupClient.sh
@@ -4,11 +4,12 @@ backupHost="server02"
 backupUser="fille"
 remotePath="/mnt/storage/home"
 localPath="/home/fille/"
+excludeList="/home/fille/src/bin/backup.exclude"
 
 echo "Starting backup..."
 echo $(date '+%Y-%m-%d %H:%M')
 echo "---------------"
-rsync -raz --update --delete $localPath $backupUser@$backupHost:$remotePath
+rsync -raz --update --delete --exclude-from $excludeList $localPath $backupUser@$backupHost:$remotePath
 echo "---------------"
 echo "Finished"
 echo $(date '+%Y-%m-%d %H:%M')
