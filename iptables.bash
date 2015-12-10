fdcffd54ef43f2f74cb8faf43cf728347f02d50b
Hans-Filip Elo <hansfilipelo@gmail.com>
2015-12-10 21:36:51 +0100

    NATed ssh, mosh and web

--- iptables.bash
+++ iptables.bash
@@ -2,6 +2,14 @@
 
 PATH=/usr/sbin:/sbin:/bin:/usr/bin
 
+#### MACHINES ####
+
+dt03=10.0.0.20
+web=10.0.0.17
+server02=10.0.0.5
+
+##################
+
 #
 # delete all existing rules.
 #
@@ -12,7 +20,6 @@ iptables -X
 
 # Always accept loopback traffic
 iptables -A INPUT -i lo -j ACCEPT
-
 
 # Allow established connections, and those not coming from the outside
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
@@ -27,7 +34,21 @@ iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
 iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
 
 # Don't forward from the outside to the inside.
-iptables -A FORWARD -i eth1 -o eth1 -j REJECT
+# iptables -A FORWARD -i eth1 -o eth0 -j REJECT
+
+# NAT port forwarding
+# port 80
+iptables -A PREROUTING -t nat -i eth1 -p tcp --dport 80 -j DNAT --to $web
+iptables -A INPUT -p tcp -m state --state NEW --dport 80 -i eth1 -j ACCEPT
+# port 443
+iptables -A PREROUTING -t nat -i eth1 -p tcp --dport 443 -j DNAT --to $web
+#iptables -A INPUT -p tcp -m state --state NEW --dport 443 -i eth1 -j ACCEPT
+# ssh
+iptables -A PREROUTING -t nat -i eth1 -p tcp --dport 22 -j DNAT --to $dt03
+iptables -A INPUT -p tcp -m state --state NEW --dport 22 -i eth1 -j ACCEPT
+# mosh
+iptables -A PREROUTING -t nat -i eth1 -p udp --dport 60000:61000 -j DNAT --to $dt03
+iptables -A INPUT -p udp -m state --state NEW --dport 60000:61000 -i eth1 -j ACCEPT
 
 # Enable routing.
 echo 1 > /proc/sys/net/ipv4/ip_forward
