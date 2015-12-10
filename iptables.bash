59906e08ee5b4f50a7177e2623638c4300df6783
Hans-Filip Elo <hansfilipelo@gmail.com>
2015-12-10 21:05:35 +0100

    NAT for iptables working

---
+++ iptables.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PATH=/usr/sbin:/sbin:/bin:/usr/bin
+
+#
+# delete all existing rules.
+#
+iptables -F
+iptables -t nat -F
+iptables -t mangle -F
+iptables -X
+
+# Always accept loopback traffic
+iptables -A INPUT -i lo -j ACCEPT
+
+
+# Allow established connections, and those not coming from the outside
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -m state --state NEW -i eth0 -j ACCEPT
+iptables -A INPUT -m state --state NEW -i lo -j ACCEPT
+iptables -A FORWARD -i eth1 -o eth0 -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Allow outgoing connections from the LAN side.
+iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
+
+# Masquerade.
+iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+
+# Don't forward from the outside to the inside.
+iptables -A FORWARD -i eth1 -o eth1 -j REJECT
+
+# Enable routing.
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
