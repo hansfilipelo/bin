9944cd8c7a921c5d8ecbf4bbec8e23078c38443b
Hans-Filip Elo <hansfilipelo@gmail.com>
2016-01-31 16:22:26 +0100

    Variables for interfaces

--- iptables.bash
+++ iptables.bash
@@ -2,6 +2,11 @@
 
 PATH=/usr/sbin:/sbin:/bin:/usr/bin
 
+#### INTERFACES ####
+
+external=eth1
+internal=eth0
+
 #### MACHINES ####
 
 dt03=10.0.0.20
@@ -30,18 +35,18 @@ iptables -X
 iptables -A INPUT -i lo -j ACCEPT
 # Allow established connections, and those not coming from the outside
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A INPUT -m state --state NEW -i eth0 -j ACCEPT
+iptables -A INPUT -m state --state NEW -i $internal -j ACCEPT
 iptables -A INPUT -m state --state NEW -i lo -j ACCEPT
-iptables -A FORWARD -i eth1 -o eth0 -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -i $external -o $internal -m state --state ESTABLISHED,RELATED -j ACCEPT
 # Allow outgoing connections from the LAN side.
-iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
+iptables -A FORWARD -i $internal -o $external -j ACCEPT
 # Masquerade - necessary for NAT
-iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o $external -j MASQUERADE
 # Enable routing.
 echo 1 > /proc/sys/net/ipv4/ip_forward
 # -------------
 # Don't forward from the outside to the inside.
-# iptables -A FORWARD -i eth1 -o eth0 -j REJECT
+# iptables -A FORWARD -i $external -o $internal -j REJECT
 
 # ---------------------------------------------
 # QoS for better performance
@@ -63,42 +68,39 @@ iptables -t mangle -A PREROUTING -p tcp --dport $ssh -j TOS --set-tos Minimize-D
 # ---------------------------------------------
 
 # port 80
-iptables -A PREROUTING -t nat -i eth1 -p tcp --dport $http -j DNAT --to $web
-iptables -A INPUT -p tcp -m state --state NEW --dport $http -i eth1 -j ACCEPT
+iptables -A PREROUTING -t nat -i $external -p tcp --dport $http -j DNAT --to $web
+iptables -A INPUT -p tcp -m state --state NEW --dport $http -i $external -j ACCEPT
 # port 443
-iptables -A PREROUTING -t nat -i eth1 -p tcp --dport $https -j DNAT --to $web
-iptables -A INPUT -p tcp -m state --state NEW --dport $https -i eth1 -j ACCEPT
+iptables -A PREROUTING -t nat -i $external -p tcp --dport $https -j DNAT --to $web
+iptables -A INPUT -p tcp -m state --state NEW --dport $https -i $external -j ACCEPT
 
 # ssh
-iptables -A PREROUTING -t nat -i eth1 -p tcp --dport $ssh -j DNAT --to $dt03
-iptables -A INPUT -p tcp -m state --state NEW --dport $ssh -i eth1 -j ACCEPT
+iptables -A PREROUTING -t nat -i $external -p tcp --dport $ssh -j DNAT --to $dt03
+iptables -A INPUT -p tcp -m state --state NEW --dport $ssh -i $external -j ACCEPT
 
 # mosh
-iptables -A PREROUTING -t nat -i eth1 -p udp --dport 60000:61000 -j DNAT --to $dt03
-iptables -A INPUT -p udp -m state --state NEW --dport 60000:61000 -i eth1 -j ACCEPT
+iptables -A PREROUTING -t nat -i $external -p udp --dport 60000:61000 -j DNAT --to $dt03
+iptables -A INPUT -p udp -m state --state NEW --dport 60000:61000 -i $external -j ACCEPT
 
 # BitTorrent
-#iptables -A PREROUTING -t nat -i eth1 -p tcp --dport 64795 -j DNAT --to $server02
-#iptables -A INPUT -p tcp -m state --state NEW --dport 64795 -i eth1 -j ACCEPT
+#iptables -A PREROUTING -t nat -i $external -p tcp --dport 64795 -j DNAT --to $server02
+#iptables -A INPUT -p tcp -m state --state NEW --dport 64795 -i $external -j ACCEPT
 
 # ---- Counter Strike server
 # 1200
-iptables -A PREROUTING -t nat -i eth1 -p udp --dport 1200 -j DNAT --to $dt03
-iptables -A INPUT -p udp -m state --state NEW --dport 1200 -i eth1 -j ACCEPT
-iptables -A PREROUTING -t nat -i eth1 -p tcp --dport 1200 -j DNAT --to $dt03
-iptables -A INPUT -p tcp -m state --state NEW --dport 1200 -i eth1 -j ACCEPT
+iptables -A PREROUTING -t nat -i $external -p udp --dport 1200 -j DNAT --to $dt03
+iptables -A INPUT -p udp -m state --state NEW --dport 1200 -i $external -j ACCEPT
+iptables -A PREROUTING -t nat -i $external -p tcp --dport 1200 -j DNAT --to $dt03
+iptables -A INPUT -p tcp -m state --state NEW --dport 1200 -i $external -j ACCEPT
 # 27000-27015
-iptables -A PREROUTING -t nat -i eth1 -p udp --dport 27000:27015 -j DNAT --to $dt03
-iptables -A INPUT -p udp -m state --state NEW --dport 27000:27015 -i eth1 -j ACCEPT
-iptables -A PREROUTING -t nat -i eth1 -p tcp --dport 27000:27015 -j DNAT --to $dt03
-iptables -A INPUT -p tcp -m state --state NEW --dport 27000:27015 -i eth1 -j ACCEPT
+iptables -A PREROUTING -t nat -i $external -p udp --dport 27000:27015 -j DNAT --to $dt03
+iptables -A INPUT -p udp -m state --state NEW --dport 27000:27015 -i $external -j ACCEPT
+iptables -A PREROUTING -t nat -i $external -p tcp --dport 27000:27015 -j DNAT --to $dt03
+iptables -A INPUT -p tcp -m state --state NEW --dport 27000:27015 -i $external -j ACCEPT
 # 27020-27039
-iptables -A PREROUTING -t nat -i eth1 -p udp --dport 27020:27039 -j DNAT --to $dt03
-iptables -A INPUT -p udp -m state --state NEW --dport 27020:27039 -i eth1 -j ACCEPT
-iptables -A PREROUTING -t nat -i eth1 -p tcp --dport 27020:27039 -j DNAT --to $dt03
-iptables -A INPUT -p tcp -m state --state NEW --dport 27020:27039 -i eth1 -j ACCEPT
+iptables -A PREROUTING -t nat -i $external -p udp --dport 27020:27039 -j DNAT --to $dt03
+iptables -A INPUT -p udp -m state --state NEW --dport 27020:27039 -i $external -j ACCEPT
+iptables -A PREROUTING -t nat -i $external -p tcp --dport 27020:27039 -j DNAT --to $dt03
+iptables -A INPUT -p tcp -m state --state NEW --dport 27020:27039 -i $external -j ACCEPT
 
 # -----------------------------
-
-
-
